0c05799bf510beeecf2c00d8e49dce6600ad185d
Wilbert Irece <wilbertirece002@gmail.com>
2024-06-22 14:46:42 +0800

    Code Updates

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -25,6 +25,7 @@ public static class SeedData
 					Title = "When Harry Met Sally",
 					ReleaseDate = DateTime.Parse("1989-2-12"),
 					Genre = "Romantic Comedy",
+					Rating = "R",
 					Price = 7.99M
 				},
 				new Movie
@@ -32,21 +33,24 @@ public static class SeedData
 					Title = "Ghostbusters ",
 					ReleaseDate = DateTime.Parse("1984-3-13"),
 					Genre = "Comedy",
-					Price = 8.99M
+                    Rating = "R",
+                    Price = 8.99M
 				},
 				new Movie
 				{
 					Title = "Ghostbusters 2",
 					ReleaseDate = DateTime.Parse("1986-2-23"),
 					Genre = "Comedy",
-					Price = 9.99M
+                    Rating = "R",
+                    Price = 9.99M
 				},
 				new Movie
 				{
 					Title = "Rio Bravo",
 					ReleaseDate = DateTime.Parse("1959-4-15"),
 					Genre = "Western",
-					Price = 3.99M
+                    Rating = "R",
+                    Price = 3.99M
 				}
 			);
 			context.SaveChanges();
